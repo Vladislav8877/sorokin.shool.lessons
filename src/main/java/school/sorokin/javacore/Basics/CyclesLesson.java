40850a30893052a43ff6a2fe52523560e63b8631
Vlad Azarov <vladazarov44@gmail.com>
2025-10-10 10:23:56 +0300

    commit - CyclesLesson

---
+++ src/main/java/school/sorokin/javacore/Basics/CyclesLesson.java
@@ -0,0 +1,74 @@
+package school.sorokin.javacore.Basics;
+
+public class CyclesLesson {
+    public static void main(String[] args) {
+
+
+
+/*
+   for(int a = 1;a<=100;a++)
+   {
+       String number = (a%3==0)?"Делится":"Не делится";    // Задание 1.
+       System.out.println(a+" - "+number);
+   }
+*/
+/*
+   int a = 10;
+   while(a>0)
+   {
+       System.out.println(a--); // Можно как и в print сделать интерацию "a--" так и за пределами print.
+   }
+*/
+
+
+/*
+        for(int a = 1; a<=9;a++)
+        {
+            for(int b = 1; b<=10;b++)
+            {
+                int total = a*b;
+                System.out.println(a+" * "+b+" = " + total); // Задание номер 3.
+            } System.out.println();
+        }
+*/
+
+/*        int a = 0;
+        while(a<=35)
+        {
+            int b = 0;
+            while(b<=35)
+            {
+                b++;
+                System.out.print(a+","+b+" ");
+                if(b==30)
+                {
+                    break;
+                }
+            } System.out.println();
+            a++;
+        }
+
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
